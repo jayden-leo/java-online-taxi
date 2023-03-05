f46a3a2c5d27e8baa1ebdb75a50fb06f9af2773b
jayden <1263346845@qq.com>
2023-03-05 11:29:35 +0800

    api-driver调用service-verifycode获取校验码功能实现

---
+++ api-driver/src/main/java/com/jayden/apidriver/remote/ServiceVerifyCodeClient.java
@@ -0,0 +1,14 @@
+package com.jayden.apidriver.remote;
+
+import com.jayden.internalcommon.dto.ResponseResult;
+import com.jayden.internalcommon.response.NumberCodeResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("service-verifycode")
+public interface ServiceVerifyCodeClient {
+
+    @GetMapping("/number-code/{size}")
+    public ResponseResult<NumberCodeResponse> verifyCodeGet(@PathVariable("size") Integer size);
+}
--- api-driver/src/main/java/com/jayden/apidriver/service/VerifyCodeService.java
+++ api-driver/src/main/java/com/jayden/apidriver/service/VerifyCodeService.java
@@ -1,10 +1,12 @@
 package com.jayden.apidriver.service;
 
 import com.jayden.apidriver.remote.ServiceDriverUserClient;
+import com.jayden.apidriver.remote.ServiceVerifyCodeClient;
 import com.jayden.internalcommon.constant.CommonStatusEnum;
 import com.jayden.internalcommon.constant.DriverCarConstants;
 import com.jayden.internalcommon.dto.ResponseResult;
 import com.jayden.internalcommon.response.DriverUserExistsResponse;
+import com.jayden.internalcommon.response.NumberCodeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,9 @@ public class VerifyCodeService {
 
     @Autowired
     private ServiceDriverUserClient serviceDriverUserClient;
+
+    @Autowired
+    private ServiceVerifyCodeClient serviceVerifyCodeClient;
 
     public ResponseResult checkAndSendVerifyCode(String driverPhone){
         // 查询 service-driver-user,该手机号的司机是否存在
@@ -25,7 +30,10 @@ public class VerifyCodeService {
         System.out.println(driverPhone+" 的司机存在 ");
 
         // 获取验证码
-        System.out.println(driverPhone);
+        ResponseResult<NumberCodeResponse> numberCodeResult = serviceVerifyCodeClient.verifyCodeGet(6);
+        NumberCodeResponse numberCodeResponse = numberCodeResult.getData();
+        Integer numberCode = numberCodeResponse.getNumberCode();
+        System.out.println(numberCode);
 
         //  调用第三方发送验证码
 
